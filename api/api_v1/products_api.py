2a7c8021393a3759ce0faede96934478a6640318
Sergey <kursevlad@yandex.ru>
2026-04-28 09:34:26 +0300

    fix: Зависимость verify_token перенесена из глобального app в api products

--- api/api_v1/products_api.py
+++ api/api_v1/products_api.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter, Depends, HTTPException
+import os
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database.config import get_db
@@ -6,7 +9,21 @@ from database.models import FakeProduct
 from schemas import FakeProductSchema
 from services import products_services as ps
 
-router = APIRouter()
+BEARER_TOKEN = os.getenv("BEARER_TOKEN")
+
+security = HTTPBearer()
+
+
+async def verify_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    """Асинхронная проверка токена"""
+    token = credentials.credentials
+    if token != BEARER_TOKEN:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Invalid token"
+        )
+    return token
+
+router = APIRouter(dependencies=[Depends(verify_token)])
 
 @router.get("/products/{product_id}")
 async def get_product_by_id(product_id: int, session: AsyncSession = Depends(get_db)):
