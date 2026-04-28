d348f0c946e131112faf090e955bd17c6c497eed
Sergey <kursevlad@yandex.ru>
2026-04-28 21:41:36 +0300

    new: added ProductCategory model

--- database/models.py
+++ database/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped, relationship
+from sqlalchemy import ForeignKey
 from sqlalchemy import JSON
 
 
@@ -6,12 +7,20 @@ class Base(DeclarativeBase):
     pass
 
 
-class FakeProduct(Base):
-    __tablename__ = 'fake_products'
+class ProductCategory(Base):
+    __tablename__ = "categories"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(nullable=False)
+    products: Mapped[list["Product"]] = relationship(back_populates="category")
+
+
+class Product(Base):
+    __tablename__ = 'products'
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(nullable=False)
     price: Mapped[int] = mapped_column(nullable=False)
     description: Mapped[str] = mapped_column(nullable=False)
-    category: Mapped[str] = mapped_column(nullable=False)
     image: Mapped[str] = mapped_column(nullable=False)
     rating: Mapped[dict] = mapped_column(JSON, nullable=False)
+    category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"), nullable=False)
+    category: Mapped["ProductCategory"] = relationship(back_populates="products")
