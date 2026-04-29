f135c16b2f205786ac3e4843ee6d72c7cec0a3e7
Sergey <kursevlad@yandex.ru>
2026-04-29 09:17:53 +0300

    fix: get_products_by_category

--- api/api_v1/products_api.py
+++ api/api_v1/products_api.py
@@ -49,6 +49,6 @@ async def get_products_by_category(
     if not items:
         raise HTTPException(status_code=404, detail="Products not found")
     return ProductListByCategory(
-        items=items[:-1],
+        items=items[:-1] if len(items) > 1 else items,
         has_more=bool(len(items) > (request.limit)), # нашлось продуктов больше, чем запрашивали в limit
     )
