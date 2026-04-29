5ee339003397f755763c2ec0e9f84088b61fea50
Sergey <kursevlad@yandex.ru>
2026-04-29 08:36:17 +0300

    new: Добавлена schema ProductListByCategory

--- schemas.py
+++ schemas.py
@@ -1,9 +1,14 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
+
+class ProductsByCategoryRequest(BaseModel):
+    category_id: int = Field(gt=0)
+    page: int  = Field(default=1, ge=1)
+    limit: int  = Field(default=20, ge=1, le=20)
 
 
 class ProductSchema(BaseModel):
+    """Продукт"""
     model_config = ConfigDict(from_attributes=True)
-
     id: int
     title: str
     price: int
@@ -11,3 +16,10 @@ class ProductSchema(BaseModel):
     category_id: int
     image: str
     rating: dict
+
+
+class ProductListByCategory(BaseModel):
+    """Список продуктов по категории."""
+    model_config = ConfigDict(from_attributes=True)
+    items: list[ProductSchema]
+    has_more: bool
