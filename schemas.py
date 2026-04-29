50284fe708abb9d43ade6d6a8ac5d06562c84753
Sergey <kursevlad@yandex.ru>
2026-04-29 18:03:08 +0300

    new: add RatingSchema

--- schemas.py
+++ schemas.py
@@ -1,6 +1,12 @@
 from pydantic import BaseModel, ConfigDict, Field
 
+class RatingSchema(BaseModel):
+    """Схема объекта rating в объекте ProductSchema"""
+    rate: float
+    count: int
+
 class ProductsByCategoryRequest(BaseModel):
+    """Запрос списка продуктов по категории"""
     category_id: int = Field(gt=0)
     page: int  = Field(default=1, ge=1)
     limit: int  = Field(default=20, ge=1, le=20)
@@ -15,7 +21,7 @@ class ProductSchema(BaseModel):
     description: str
     category_id: int
     image: str
-    rating: dict
+    rating: RatingSchema
 
 
 class ProductListByCategory(BaseModel):
