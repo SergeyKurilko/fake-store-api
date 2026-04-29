ae4944b4f2ae8b6dddd1867ab50c0d4208a8f4f4
Sergey <kursevlad@yandex.ru>
2026-04-29 08:37:08 +0300

    new: Добавлен get_product_by_id в слой repository

--- repository/products_repo.py
+++ repository/products_repo.py
@@ -2,8 +2,21 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from database.models import Product
 
+
 async def get_product_by_id(session: AsyncSession, product_id: int) -> Product | None:
     """Асинхронное получение продукта по ID"""
     stmt = select(Product).where(Product.id == product_id)
     result = await session.execute(stmt)
     return result.scalar_one_or_none()
+
+
+async def get_products_by_category_and_page(
+    session: AsyncSession, category_id: int, page: int, limit: int
+):
+    """Получения списка продуктов по category_id и page (offset = page * 20)"""
+    stmt = select(Product).where(Product.category_id == category_id)
+
+    offset = (page - 1) * limit
+    stmt = stmt.offset(offset).limit(limit + 1)
+    result = await session.execute(stmt)
+    return result.scalars().all()
